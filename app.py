753d4654ae61c0498527ac5439193c611e8460c8
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-01-07 08:37:44 +0200

    Making it work

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 import sys
-
+import os
 from django.conf import settings
+
+BASE_DIR = os.path.dirname(__file__)
 
 settings.configure(
     DEBUG=True,
     SECRET_KEY="very_secret",
-    ROOT_URLCONF="site.urls",
+    ROOT_URLCONF="main.urls",
     MIDDLEWARE_CLASSES=(),
     INSTALLED_APPS=(
         "django.contrib.staticfiles",
-        "site"
+        "main",
     ),
     TEMPLATES=(
         {
             "BACKEND": "django.template.backends.django.DjangoTemplates",
             "DIRS": [],
-            "APP_DIRS": True
+            "APP_DIRS": True,
         },
     ),
-    STATIC_URL="/static/"
+    STATIC_URL="/static/",
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, "content"),
 )
 
 if __name__ == "__main__":
